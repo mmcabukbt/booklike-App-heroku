5c04f4079c0dc26bc16a5f8ede653892bc2425a4
mmcabukbt <mmcabukbt@protonmail.com>
2022-02-22 01:39:43 +0300

    baseurl update

--- src/services/axios/index.js
+++ src/services/axios/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 // const port = (process && process.env.PORT) || 3030;
 export const axiosBase = axios.create({
-	baseURL: `https://booklike-app-jsdb-heroku.herokuapp.com//api`,
+	baseURL: `https://booklike-app-jsdb-heroku.herokuapp.com/api`,
 })
